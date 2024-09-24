8c2b1e80cc4745249f81eb5d34befb213b0b97b1
Ridgy8 <113268503+Ridgy8@users.noreply.github.com>
2024-09-24 17:21:12 -0400

    Add files via upload

---
+++ src/BuddyInfo.java
@@ -0,0 +1,28 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phone;
+
+    public BuddyInfo(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddyInfo = new BuddyInfo("Homer", "123 Main st.", "123456789");
+        System.out.println("Hello " + buddyInfo.getName());
+    }
+}
